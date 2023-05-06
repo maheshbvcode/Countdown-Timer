695c445067da23dc933889d663a02bf7055ef873
Mahesh BV <76767461+maheshbvcode@users.noreply.github.com>
2023-05-06 13:27:33 +0530

    Update TimerInt

--- src/component/TimerInt.js
+++ src/component/TimerInt.js
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 const TimerInt = ({onReqTimerStart}) => {
-   
+   const [timeInSec, setTimeInSec] = useState('');
   return (
     <div>
         <label htmlFor='time-count'>Enter Time in seconds</label>
         <input type='number' id='time-count' placeholder='Enter Time in secs'
+        value={timeInSec}
+        onChange={(e) => setTimeInSec(e.target.value)}
         onKeyDown={(e)=>{
             // console.log(e)
-            if(e.keyCode===13 || e.which===13){
-                onReqTimerStart && onReqTimerStart(Math.floor(e.target.value))
+            if (e.keyCode === 13 || e.which === 13) {
+              onReqTimerStart && onReqTimerStart(Math.floor(timeInSec));
+              setTimeInSec('');
             }
 
         }}
